c7ed75fc1754679f5fa3377e451580b85e971035
Sam Caldwell <github@samcaldwell.net>
2025-06-04 21:35:44 -0500

    add utils.ValidIdentifier()

---
+++ pkg/utils/ValidIdentifier.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"fmt"
+	"regexp"
+)
+
+// ValidIdentifier - validating the identifier pattern
+func ValidIdentifier(s string) error {
+
+	const identifierPattern = "^[a-zA-Z][a-zA-Z0-9\\_\\-\\.]{0-63}[a-zA-Z0-9]$"
+
+	if pattern := regexp.MustCompile(identifierPattern); pattern.MatchString(s) {
+
+		return nil
+
+	}
+
+	return fmt.Errorf("identifier (%s) invalid.  Must match %s", s, identifierPattern)
+
+}
