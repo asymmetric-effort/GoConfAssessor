815b7fb89a165192a8b754c4662809a7f930ea0e
Sam Caldwell <github@samcaldwell.net>
2025-06-04 20:56:01 -0500

    add AssertionGroup struct

--- pkg/manifest/AssertionGroup.go
+++ pkg/manifest/AssertionGroup.go
@@ -1,10 +1,15 @@
-// file: pkg/manifest/Group.go
+// file: pkg/manifest/AssertionGroup.go
 // (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 
 package manifest
 
-// Group represents a named collection of assertions.
-type Group struct {
-	Name  string
-	Items []Assertion
+// AssertionGroup represents a named collection of assertions.
+type AssertionGroup struct {
+	Include string `yaml:"include,omitempty"`
+
+	Name string `yaml:"name,omitempty"`
+
+	Parser string `yaml:"parser,omitempty"`
+
+	Items []Assertion `yaml:"items,omitempty"`
 }
