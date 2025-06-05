88511a1ebde6d829789e89eb0cec8b936a58d374
Sam Caldwell <github@samcaldwell.net>
2025-06-04 20:10:43 -0500

    manifest-verify/main.go

--- pkg/utils/LoadYaml.go
+++ pkg/utils/LoadYaml.go
@@ -6,15 +6,15 @@ import (
 	"os"
 )
 
-func LoadYaml(fileName *string, yamlObject interface{}) (err error) {
+func LoadYaml(fileName string, yamlObject interface{}) (err error) {
 	var data []byte
 
-	if data, err = os.ReadFile(*fileName); err != nil {
-		return fmt.Errorf("failed to read file %s: %w", *fileName, err)
+	if data, err = os.ReadFile(fileName); err != nil {
+		return fmt.Errorf("failed to read file %s: %w", fileName, err)
 	}
 
 	if err = yaml.Unmarshal(data, &yamlObject); err != nil {
-		return fmt.Errorf("failed to unmarshal YAML %s: %w", *fileName, err)
+		return fmt.Errorf("failed to unmarshal YAML %s: %w", fileName, err)
 	}
 
 	return err
