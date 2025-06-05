c5a149dfa2587a004cec3bfc577e993a879f605f
Sam Caldwell <github@samcaldwell.net>
2025-06-04 22:08:33 -0500

    simplify parser validation

--- pkg/parsers/parserMap.go
+++ pkg/parsers/parserMap.go
@@ -1,5 +1,10 @@
 package parsers
 
+import (
+	"fmt"
+	"strings"
+)
+
 type ParserMap map[string]Parser
 
 var SupportedParsers = ParserMap{
@@ -11,4 +16,12 @@ var SupportedParsers = ParserMap{
 	"json":      NotImplemented{},
 	"ini":       NotImplemented{},
 	"text":      NotImplemented{},
+}
+
+func IsValidParser(p string) error {
+	thisParser := strings.ToLower(strings.TrimSpace(p))
+	if _, ok := SupportedParsers[thisParser]; !ok {
+		return fmt.Errorf("unknown or unsupported parser (%s)", thisParser)
+	}
+	return nil
 }
